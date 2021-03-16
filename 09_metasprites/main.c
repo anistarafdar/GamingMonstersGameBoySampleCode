c1d51fe03845b4fd639aabbe3c2c0deff628ac7f
Anis <59984748+anistarafdar@users.noreply.github.com>
2021-03-15 22:15:09 -0400

    updated functions to work with new struct def and replaced repetitive code
    updated syntax for portions of code using the old version of the struct
    
    there was two functions that did the same thing so i condensed it into a reusable function that can be used for any 16x16 meta sprite GameCharacter

--- 09_metasprites/main.c
+++ 09_metasprites/main.c
@@ -3,66 +3,47 @@
 #include "GameCharacter.c"
 #include "GameSprites.c"
 
-struct GameCharacter ship;
-struct GameCharacter bug;
-UBYTE spritesize = 8;
+#define SPRITE_SIZE 8
+
+GameCharacter ship;
+GameCharacter bug;
 
 void performantdelay(UINT8 numloops){
-    UINT8 i;
-    for(i = 0; i < numloops; i++){
+    for(UINT8 i = 0; i < numloops; i++){
         wait_vbl_done();
     }     
 }
 
-void movegamecharacter(struct GameCharacter* character, UINT8 x, UINT8 y){
+void movegamecharacter(GameCharacter* character, UINT8 x, UINT8 y){
+    // implies that four tiles from the value of tile_start and up are for the same meta sprite
+    // and are arranged clockwise, starting from the top left 8x8 quadrant
     move_sprite(character->spritids[0], x, y);
-    move_sprite(character->spritids[1], x + spritesize, y);
-    move_sprite(character->spritids[2], x, y + spritesize);
-    move_sprite(character->spritids[3], x + spritesize, y + spritesize);
+    move_sprite(character->spritids[1], x + SPRITE_SIZE, y);
+    move_sprite(character->spritids[2], x, y + SPRITE_SIZE);
+    move_sprite(character->spritids[3], x + SPRITE_SIZE, y + SPRITE_SIZE);
 }
 
-void setupship(){
-    ship.x = 80;
-    ship.y = 130;
-    ship.width = 16;
-    ship.height = 16;
+void setup_GameCharacter(GameCharacter* character, UINT8 x_position, UINT8 y_position, UINT8 tile_start){
+    character->x = x_position;
+    character->y = y_position;
 
-    // load sprites for ship
-    set_sprite_tile(0, 0);
-    ship.spritids[0] = 0;
-    set_sprite_tile(1, 1);
-    ship.spritids[1] = 1;
-    set_sprite_tile(2, 2);
-    ship.spritids[2] = 2;
-    set_sprite_tile(3, 3);
-    ship.spritids[3] = 3;
+    // load sprites for sprite
+    // starts reading from tile_start and increments up by one to retrieve sprite ids
+    for(UINT8 i=0; i<4; i++) {
+		set_sprite_tile(tile_start, tile_start);
+        character->spritids[i] = tile_start;
 
-    movegamecharacter(&ship, ship.x, ship.y);
-}
+        tile_start++;
+	}
 
-void setupbug(){
-    bug.x = 30;
-    bug.y = 0;
-    bug.width = 16;
-    bug.height = 16;    
-
-    // load sprites for bug
-    set_sprite_tile(4, 4);
-    bug.spritids[0] = 4;
-    set_sprite_tile(5, 5);
-    bug.spritids[1] = 5;
-    set_sprite_tile(6, 6);
-    bug.spritids[2] = 6;
-    set_sprite_tile(7, 7);
-    bug.spritids[3] = 7;
-
-    movegamecharacter(&bug, bug.x, bug.y);
+    movegamecharacter(character, character->x, character->y);
 }
 
 void main(){
     set_sprite_data(0, 8, GameSprites);
-    setupship();
-    setupbug();
+
+    setup_GameCharacter(&ship, 80, 130, 0);
+    setup_GameCharacter(&bug, 30, 0, 4);
 
     SHOW_SPRITES;
     DISPLAY_ON;
